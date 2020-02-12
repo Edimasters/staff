67194736d1df933ea00c24764869249f4d426034
Edimasters <eedzhagaryan@gmail.com>
2020-02-12 10:21:02 +0500

    Updates

---
+++ precache-manifest.60bc55213d61a4b0813db42ac087487d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a36a682e48d7dbb5cabbe9418b26454c",
+    "url": "/staff/index.html"
+  },
+  {
+    "revision": "d8577e25479dde453d7c",
+    "url": "/staff/static/css/2.08c74b9a.chunk.css"
+  },
+  {
+    "revision": "503ccb85e77c47e342d5",
+    "url": "/staff/static/css/main.af4d417a.chunk.css"
+  },
+  {
+    "revision": "d8577e25479dde453d7c",
+    "url": "/staff/static/js/2.33091725.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/staff/static/js/2.33091725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "503ccb85e77c47e342d5",
+    "url": "/staff/static/js/main.78077602.chunk.js"
+  },
+  {
+    "revision": "ebc488e69edacbd61808",
+    "url": "/staff/static/js/runtime-main.787cb266.js"
+  }
+]);
